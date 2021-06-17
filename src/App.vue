08c0054a298451919fb673fd2b4f5216fac3deec
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-06-17 10:28:24 +0300

    Base reset style - done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,60 @@
 <template>
-  <Header></Header>
+  <Authorization></Authorization>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
+import Authorization from "./pages/Authorization.vue";
 export default {
   components: {
-    Header,
+    Authorization,
   },
 };
 </script>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+html {
+  &::after,
+  &::before {
+    box-sizing: inherit;
+  }
+}
+body {
+  font-family: Arimo, sans-serif;
+  img {
+    display: block;
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+  .visually-hidden {
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: -1px;
+    border: 0;
+    padding: 0;
+    white-space: nowrap;
+    clip-path: inset(100%);
+    clip: rect(0 0 0 0);
+    overflow: hidden;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p {
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+}
 </style>
---
+++ src/pages/Authorization.vue
@@ -0,0 +1,31 @@
+<Header></Header>
+  <section class="subheader">
+    <div class="subheader__title"><span>השמ םייח</span> ,םולש</div>
+  </section>
+
+  <style lang="scss">
+.subheader {
+  position: relative;
+  display: none;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0 62px;
+  height: 76px;
+  background-color: #f5f5f5;
+  box-shadow: inset 0 14px 16px -16px rgba(0, 0, 0, 0.4);
+}
+
+@media (min-width: 1330px) {
+  .subheader {
+    display: flex;
+    &__title {
+      text-align: right;
+      font-size: 16px;
+      line-height: 18px;
+      span {
+        font-weight: bold;
+      }
+    }
+  }
+}
+</style>
